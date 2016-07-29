da72b00c306480697e0628027aad0a6a9de06243
necoline <hnhubner@gmail.com>
2016-07-28 18:05:09 -0600

    up to 8

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//1
+//1 sum of two numbers
 var num1;
 var num2;
 
@@ -7,20 +7,20 @@ function sum(num1,num2) {
 }
 
 
-//2
+//2 sum of array
 var array = [2,4,5,9,12];
 var sum = 0;
 for(var i = 0; i < array.length; i++) {
   sum += array[i];
 }
 
-//3
+//3 F - C
 function convert(fTemp) {
     return ((fTemp - 32) * (5/9));
 }
 
 
-//4
+//4 vowel count
 var string = "this is a vowel count";
 var vowelCount = 0;
 for (var i = 0; i < string.length; i++) {
@@ -30,7 +30,7 @@ for (var i = 0; i < string.length; i++) {
 }
 
 
-//5
+//5 roll 2 dice
 var dice = ["1", "2", "3", "4", "5", "6"];
 function roll() {
   var score = dice[Math.floor(Math.random()*dice.length)];
@@ -38,15 +38,36 @@ function roll() {
 }
 console.log("roll", [roll(), roll()]);
 
-//6
-var avgAr = [3, 5, 7, 6, 6, 3, 1, 2, 9];
-function 
+
+//6 array averag
+var numbers = [3, 5, 7, 6, 6, 3, 1, 2, 9];
+var sum = 0;
+var avg = 0;
+
+for (var i = 0; i < numbers.length; i++) {
+  sum += (numbers[i]);
+  avg = (sum/numbers.length);
+}
+console.log("the average of the array is ", avg);
+
+
+//7 - FizzBuzz
+for (var i = 0; i <= 100; i++) {
+  console.log(((["fizz "][i%3] || " ") + (["buzz "] [i%5] || " ") + i);
+}
+
+
+//8 factorial
+function factorial(n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+console.log (factorial(4));
 
 
 
 
 
-
-
-
-//7
+//9
